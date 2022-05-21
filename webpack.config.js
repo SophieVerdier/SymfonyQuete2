9a6fd35362568333d101dc1baefe28b21827fb70
SOPHIE VERDIER <sophie.verdier188@gmail.com>
2022-05-21 14:21:25 +0200

    added navbar + encore

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,19 @@ Encore
     // directory where compiled assets will be stored
     .setOutputPath('public/build/')
     // public path used by the web server to access the output path
+        .copyFiles({
+               from: './assets/images',
+        
+                 // optional target path, relative to the output dir
+                 to: 'images/[path][name].[ext]',
+        
+                 // if versioning is enabled, add the file hash too
+                //to: 'images/[path][name].[hash:8].[ext]',
+        
+                 // only copy files matching this pattern
+                 //pattern: /\.(png|jpg|jpeg)$/
+             })
+    
     .setPublicPath('/build')
     // only needed for CDN's or sub-directory deploy
     //.setManifestKeyPrefix('build/')
@@ -55,8 +68,8 @@ Encore
         config.corejs = 3;
     })
 
-    // enables Sass/SCSS support
-    //.enableSassLoader()
+    ///enables Sass/SCSS support
+    .enableSassLoader()
 
     // uncomment if you use TypeScript
     //.enableTypeScriptLoader()
@@ -72,4 +85,19 @@ Encore
     //.autoProvidejQuery()
 ;
 
-module.exports = Encore.getWebpackConfig();
+//supprime la dernière ligne
+// module.exports = Encore.getWebpackConfig();
+
+// et remplace par ceci
+const fullConfig = Encore.getWebpackConfig();
+fullConfig.devServer = {
+    headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+        'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization',
+    },
+    watchFiles: {
+        paths: ['templates/**/*.html.twig'],
+    },
+};
+module.exports = fullConfig;
